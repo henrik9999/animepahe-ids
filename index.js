45642af36fc667578d2c0a8410e7d130d884897f
henrik9999 <22085664+henrik9999@users.noreply.github.com>
2021-09-08 22:59:47 +0200

    only add new titles

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ import path from "path";
 import fs from "fs";
 
 async function start(){
+
+    let currentData = JSON.parse(
+        fs.readFileSync(path.resolve("data.json"), { encoding: "utf8" })
+    );
+    const currentTitles = Object.values(currentData).map(i => i.title);
+
     const response = await fetch("https://animepahe.com/anime", {headers: {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"}})
     const body = await response.text();
     const $ = cheerio.load(body);
 
-    const animes = $('a[href^=/anime/]').map(function(i,el) { return $(el).attr('href') }).get();
-    console.log(animes.length);
-    
-    const data = {};
+    const animes = $('a[href^=/anime/]').map(function(i,el) { if(!currentTitles.includes($(el).attr("title").trim())) return $(el).attr('href') }).get();
+
     for(const anime of animes) {
         console.log(anime);
         const response = await fetch("https://animepahe.com" + anime, {headers: {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"}})
@@ -28,16 +32,13 @@ async function start(){
 
         let title = $('.title-wrapper i').first().attr('title').replace('Bookmark', '').trim();
         if (!title) title = $('.title-wrapper h1').contents().get(0).nodeValue;
-        data[id] = {title}
+        currentData[id] = {title}
     }
 
-    console.log(Object.keys(data).length);
-
-    if(Object.keys(data).length > 4100) {
-        fs.writeFileSync(path.resolve("data.json"), JSON.stringify(data), {
-            encoding: "utf8",
-        });
-    }
+    console.log(Object.keys(currentData).length);
+    fs.writeFileSync(path.resolve("data.json"), JSON.stringify(currentData), {
+        encoding: "utf8",
+    });
 }
 
 start();
