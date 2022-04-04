1ce954fb78de4e10156dfe2f8fb9a90f3e0ee3d3
henrik9999 <22085664+henrik9999@users.noreply.github.com>
2022-04-04 20:33:10 +0200

    changes

--- index.js
+++ index.js
@@ -3,16 +3,26 @@ import fs from 'node:fs';
 import cheerio from 'cheerio';
 import puppeteer from 'puppeteer-extra';
 import pluginStealth from 'puppeteer-extra-plugin-stealth';
-import pluginAdblocker from 'puppeteer-extra-plugin-adblocker';
+import pluginAdblocker from 'puppeteer-extra-plugin-adblocker'
+
+puppeteer.use(pluginStealth());
+puppeteer.use(pluginAdblocker());
 
 async function start() {
+	const browser = await puppeteer.launch({headless: true});
+	const page = await browser.newPage();
+
 	const currentData = JSON.parse(
 		fs.readFileSync(path.resolve('data.json'), {encoding: 'utf8'}),
 	);
 	const currentTitles = new Set(Object.values(currentData).map(i => i.title));
-
-	const $ = await request('https://animepahe.com/anime');
-
+	
+	await page.goto('https://animepahe.com/anime', {
+		waitUntil: 'networkidle2',
+	});
+	await page.waitForSelector('div.show a[title]',{timeout: 30000}); 
+	let bodyHTML = await page.evaluate(() => document.documentElement.outerHTML);
+	let $ = cheerio.load(bodyHTML);
 	const elements = $('a[href^=/anime/]');
 
 	if(!elements.length) {
@@ -28,7 +38,13 @@ async function start() {
 
 	for (const anime of animes) {
 		console.log(anime);
-		const $ = await request('https://animepahe.com' + anime);
+		await wait(Math.floor(Math.random() * 2000) + 1);
+		await page.goto('https://animepahe.com' + anime, {
+			waitUntil: 'networkidle2',
+		});
+		await page.waitForSelector('#modalBookmark a',{timeout:10000}); 
+		bodyHTML = await page.evaluate(() => document.documentElement.outerHTML);
+		$ = cheerio.load(bodyHTML);
 
 		let id;
 		const pahe = $('a[href^="//pahe.win/a/"]').attr('href');
@@ -41,45 +57,17 @@ async function start() {
 		const title = $('a[href^="//pahe.win/a/"]').attr('title');
 		currentData[id] = {title};
 	}
-
+	await browser.close();
 	console.log(Object.keys(currentData).length);
 	fs.writeFileSync(path.resolve('data.json'), JSON.stringify(currentData), {
 		encoding: 'utf8',
 	});
 }
 
-puppeteer.use(pluginStealth());
-puppeteer.use(pluginAdblocker());
-
-let browser;
-let browserTimeout;
-
-async function request(url) {
-	if (!browser) {
-		browser = puppeteer.launch({headless: true});
-	}
-
-	browser = await browser;
-	clearTimeout(browserTimeout);
-	browserTimeout = setTimeout(async () => {
-		await browser.close();
-		browser = null;
-	}, 60 * 1000);
-	const page = await browser.newPage();
-	try {
-		const response = await page.goto(url, {
-			waitUntil: 'networkidle2',
-		});
-		const bodyHTML = await page.evaluate(() => document.documentElement.outerHTML);
-		await page.close();
-		if (Number.parseInt(response.status()) > 499 && Number.parseInt(response.status()) < 600) {
-			throw new Error(response.request.res.responseUrl);
-		}
-		return cheerio.load(bodyHTML);
-	} catch (error) {
-		await page.close();
-		throw error;
-	}
+function wait(ms) {
+	return new Promise(resolve => {
+	  setTimeout(resolve, ms);
+	});
 }
 
 start();
